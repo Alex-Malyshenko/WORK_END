69348aa81be736140bcf0c5e9753ad2fb9bc70d9
Alex <alis_mal@mail.ru>
2023-02-28 22:39:04 +0300

    The task is finished

--- TASK/Program.cs
+++ TASK/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] MassivIn;
+Console.WriteLine("Введите массив строк через пробел:");
+string stroka = Console.ReadLine();
+MassivIn = stroka.Split(' ');
+string[] MassivOut = new string[MassivIn.Length];
+
+int indexOut = 0;
+for (int index = 0; index < MassivIn.Length; index++)
+{
+    if (MassivIn[index].Length <= 3)
+    {
+        MassivOut[indexOut] = MassivIn[index];
+        indexOut++;
+    }
+}
+
+for (int i = 0; i < MassivOut.Length; i++)
+{
+    Console.WriteLine(MassivOut[i]);
+}
+
